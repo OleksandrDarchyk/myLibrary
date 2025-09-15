32d3a48177689edd4e480e206debd45fd9d65345
OleksandrDarchyk <oledar01@easv365.dk>
2025-09-15 17:57:11 +0200

    Add instanse of MyDbContext gitignore DbConnectionString

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,31 @@
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add instanse of MyDbContext 
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    var conn = builder.Configuration["AppOptions:ConnectionString"];
+    conf.UseNpgsql(conn);
+    // conf.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Root endpoint
+app.MapGet("/", () => " API is running. Try /authors, /books, /genres or /swagger");
+
+// Endpoints
+app.MapGet("/authors", ([FromServices]MyDbContext dbContext) =>
+    dbContext.Authors.ToList()
+    );
+app.MapGet("/books", ([FromServices]MyDbContext dbContext) =>
+    dbContext.Books.ToList()
+);
+app.MapGet("/genres", ([FromServices]MyDbContext dbContext) =>
+    dbContext.Genres.ToList()
+);
 
 app.Run();
